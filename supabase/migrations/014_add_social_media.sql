ff593ec6840d42b04ebfd5f3ebafdbc7f09b84be
a.perez <a.perez@neurologiq.de>
2025-08-07 15:13:58 +0200

    clean up

--- supabase/migrations/014_add_social_media.sql
+++
@@ -1,9 +0,0 @@
--- Add social media column to doctors table
-ALTER TABLE doctors 
-ADD COLUMN IF NOT EXISTS social_media JSONB DEFAULT '{}';
-
--- Add comment for documentation
-COMMENT ON COLUMN doctors.social_media IS 'JSON object containing social media links (instagram, twitter, facebook, linkedin)';
-
--- Create index for better performance when querying social media
-CREATE INDEX IF NOT EXISTS idx_doctors_social_media ON doctors USING GIN (social_media);
